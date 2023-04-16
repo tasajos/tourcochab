ad3a3eb8f5170b2b29daeaa7246cf731ae4f5ef8
tasajos <tasajos@gmail.com>
2023-04-16 19:53:59 -0400

    Version 2 habilitar whatsapp

--- app/src/main/java/org/tourcocha/chakuy/MainActivity.java
+++ app/src/main/java/org/tourcocha/chakuy/MainActivity.java
@@ -2,7 +2,15 @@ package org.tourcocha.chakuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.net.Uri;
+
+import java.net.URLEncoder;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +19,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void openWhatsApp(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://api.whatsapp.com/send?phone=70776212"));
+        startActivity(intent);
+    }
+
 }
